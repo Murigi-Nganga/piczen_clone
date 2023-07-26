92e8519be7bb73be2814afed0f22c083d6fcd6ee
Murigi-Nganga <murigigracious@students.uonbi.ac.ke>
2023-07-26 12:27:34 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.piczen_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:piczen_clone/app/app.dart';
+import 'package:piczen_clone/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
